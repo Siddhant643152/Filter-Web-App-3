aa59579d88b18b118f8f688f09461df076c8a772
Siddhant643152 <81233517+Siddhant643152@users.noreply.github.com>
2022-06-01 17:58:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+mushtachX=0;
+mushtachY=0;
+
+function preload() {
+    mushtach = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
+}
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x: " + results[0].pose.nose.x);
+        mushtachX = results[0].pose.nose.x-30;
+        console.log("nose y: " + results[0].pose.nose.y);
+        mushtachY = results[0].pose.nose.y; 
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mushtach,mushtachX,mushtachY,60,45);
+}
+function take_snapshot() {
+    save("my_picture.png");
+}
